e6afa5b955c6a544b08ae1be454d5294644c5045
Sebastian Reyes <juansreyes.salinas@gmail.com>
2023-04-14 16:01:50 -0500

    ajuste

--- src/components/global-styles.jsx
+++ src/components/global-styles.jsx
@@ -11,8 +11,16 @@ const GlobalStylesStyled = createGlobalStyle`
     padding: 0;
     font: var(--body-regular);
     background-color: #8EC5FC;
-    min-block-size: 100vh;
+    /* min-block-size: 100vh; */
     background-image: linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 100%);
+
+    @media screen and (min-width: 752px) {
+      &{
+        gap: 2rem;
+        align-items: center;
+        min-block-size: 100vh;
+      }
+    }
 
   }
 
